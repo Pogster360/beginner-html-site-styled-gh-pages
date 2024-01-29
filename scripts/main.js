d7d5301c7ff9faabfbfa7c2a1cff95580dca9aae
Pogster360 <petergregorczyk@gmail.com>
2024-01-29 15:11:51 +1100

    Initial commit

---
+++ scripts/main.js
@@ -0,0 +1,3 @@
+// First I need to initialise the scripts in html with the script cmd just before the body tag ends
+const myHeading = document.querySelector("h1");
+myHeading.textContent = "Hello World"
