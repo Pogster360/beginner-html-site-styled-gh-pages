85a72b969e1d9de7006506a2d7996cfdb1d0ec26
Pogster360 <petergregorczyk@gmail.com>
2024-01-29 15:20:59 +1100

    basic edits to html
    using javascript to edit the HTML elemtns

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,16 @@
 // First I need to initialise the scripts in html with the script cmd just before the body tag ends
+
+// This creates a constant function that will find the h1 tag in html
+// you can use tags of all types you can also use id to define which tags you want to use
 const myHeading = document.querySelector("h1");
-myHeading.textContent = "Hello World"
+
+// this will set the text in h1 to Hello World
+myHeading.textContent = "Big Header";
+
+// my practice with querySelector
+const myTitle = document.querySelector("title");
+myTitle.textContent = "Hello World";
+
+// added my own paragraph with id element for testing purposes
+const myParagraph = document.getElementById("test");
+myParagraph.textContent = "I get to add my own text here using javascript instead of html";
