1e661e6ce28aa9d441d140c4064eb49ce8a8f71d
Pogster360 <petergregorczyk@gmail.com>
2024-01-29 15:40:29 +1100

    img switcher function
    basic function switching from firefox-icon.png and firefox2.png

---
+++ scripts/example.js
@@ -0,0 +1,47 @@
+// First I need to initialise the scripts in html with the script cmd just before the body tag ends
+
+// This creates a constant function that will find the h1 tag in html
+// you can use tags of all types you can also use id to define which tags you want to use
+const myHeading = document.querySelector("h1");
+
+// this will set the text in h1 to Hello World
+myHeading.textContent = "Big Header";
+
+// my practice with querySelector
+const myTitle = document.querySelector("title");
+myTitle.textContent = "Hello World";
+
+// added my own paragraph with id element for testing purposes
+const myParagraph = document.getElementById("test");
+// myParagraph.textContent = "I get to add my own text here using javascript instead of html";
+
+// JavaScript can be run with out using html elements, it will just run the function and display as a pop up before the html loads
+// simple function to test if iceCream = chocolate
+// let iceCream = "chocolate";
+// if (iceCream === "chocolate") {
+//     alert("yay it is chocolate ice cream");
+// } else {
+//     alert("This is not chocolate ice cream");
+// }
+
+// // functions run in order until all functions are completed
+// iceCream = "vanilla"
+// if (iceCream === "chocolate") {
+//     alert("yay it is chocolate ice cream");
+// } else {
+//     alert("This is not chocolate ice cream");
+// }
+
+// creating a simple function with multiple variables
+function simpleEquation(num1, num2, num3) {
+    let result = (num1 + num2) * num3 - num1;   
+    return result;
+}
+
+myParagraph.textContent = "The answer to (12 + 7) * 4 - 12 = " + simpleEquation(12, 7, 4);
+
+// The eventlistener waits for a certain action to be perforemed before completing the action
+// In this event it will create an alert when the webpage is clicked on
+document.querySelector("html").addEventListener("click", () => {
+    alert("ouch, dont poke me");
+});
--- scripts/main.js
+++ scripts/main.js
@@ -1,47 +1,12 @@
-// First I need to initialise the scripts in html with the script cmd just before the body tag ends
+// creating the function for myImage
+const myImage = document.querySelector("img");
 
-// This creates a constant function that will find the h1 tag in html
-// you can use tags of all types you can also use id to define which tags you want to use
-const myHeading = document.querySelector("h1");
-
-// this will set the text in h1 to Hello World
-myHeading.textContent = "Big Header";
-
-// my practice with querySelector
-const myTitle = document.querySelector("title");
-myTitle.textContent = "Hello World";
-
-// added my own paragraph with id element for testing purposes
-const myParagraph = document.getElementById("test");
-// myParagraph.textContent = "I get to add my own text here using javascript instead of html";
-
-// JavaScript can be run with out using html elements, it will just run the function and display as a pop up before the html loads
-// simple function to test if iceCream = chocolate
-// let iceCream = "chocolate";
-// if (iceCream === "chocolate") {
-//     alert("yay it is chocolate ice cream");
-// } else {
-//     alert("This is not chocolate ice cream");
-// }
-
-// // functions run in order until all functions are completed
-// iceCream = "vanilla"
-// if (iceCream === "chocolate") {
-//     alert("yay it is chocolate ice cream");
-// } else {
-//     alert("This is not chocolate ice cream");
-// }
-
-// creating a simple function with multiple variables
-function simpleEquation(num1, num2, num3) {
-    let result = (num1 + num2) * num3 - num1;   
-    return result;
-}
-
-myParagraph.textContent = "The answer to (12 + 7) * 4 - 12 = " + simpleEquation(12, 7, 4);
-
-// The eventlistener waits for a certain action to be perforemed before completing the action
-// In this event it will create an alert when the webpage is clicked on
-document.querySelector("html").addEventListener("click", () => {
-    alert("ouch, dont poke me");
-});
+// This will loop between images depended on what the img is at the curret time
+myImage.onclick = () => {
+    const mySrc = myImage.getAttribute("src");
+    if (mySrc === "images/firefox-icon.png") {
+        myImage.setAttribute("src", "images/firefox2.png");
+    } else {
+        myImage.setAttribute("src", "images/firefox-icon.png");
+    }
+};
