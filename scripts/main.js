d6e402bbd78f2e1251db09baf69987cc5cd6d1a5
Pogster360 <petergregorczyk@gmail.com>
2024-01-29 15:32:25 +1100

    simple function
    creating a function to display an answer to a static mathematical equation

--- scripts/main.js
+++ scripts/main.js
@@ -13,21 +13,29 @@ myTitle.textContent = "Hello World";
 
 // added my own paragraph with id element for testing purposes
 const myParagraph = document.getElementById("test");
-myParagraph.textContent = "I get to add my own text here using javascript instead of html";
+// myParagraph.textContent = "I get to add my own text here using javascript instead of html";
 
 // JavaScript can be run with out using html elements, it will just run the function and display as a pop up before the html loads
 // simple function to test if iceCream = chocolate
-let iceCream = "chocolate";
-if (iceCream === "chocolate") {
-    alert("yay it is chocolate ice cream");
-} else {
-    alert("This is not chocolate ice cream");
+// let iceCream = "chocolate";
+// if (iceCream === "chocolate") {
+//     alert("yay it is chocolate ice cream");
+// } else {
+//     alert("This is not chocolate ice cream");
+// }
+
+// // functions run in order until all functions are completed
+// iceCream = "vanilla"
+// if (iceCream === "chocolate") {
+//     alert("yay it is chocolate ice cream");
+// } else {
+//     alert("This is not chocolate ice cream");
+// }
+
+// creating a simple function with multiple variables
+function simpleEquation(num1, num2, num3) {
+    let result = (num1 + num2) * num3 - num1;   
+    return result;
 }
 
-// functions run in order until all functions are completed
-iceCream = "vanilla"
-if (iceCream === "chocolate") {
-    alert("yay it is chocolate ice cream");
-} else {
-    alert("This is not chocolate ice cream");
-}
+myParagraph.textContent = "The answer to (12 + 7) * 4 - 12 = " + simpleEquation(12, 7, 4)
